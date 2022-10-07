39058a2c4ea299398d5fb4e21dac0a4a20630271
marguerite <i@marguerite.su>
2022-10-07 12:42:51 +0800

    do not alias OTC

--- lib/cjk.go
+++ lib/cjk.go
@@ -11,7 +11,6 @@ func GenCJKConfig(availFonts ft.Collection, userMode bool) {
 	conf := GetFcConfig("cjk", userMode)
 	text := genFcPreamble(userMode, "")
 	text += fixDualAsianFonts(availFonts, userMode)
-	text += aliasNotoCJKOTC(availFonts)
 	text += FcSuffix
 	overwriteOrRemoveFile(conf, []byte(text))
 }
@@ -51,32 +50,6 @@ func fixDualAsianFonts(availFonts ft.Collection, userMode bool) string {
 		}
 	}
 
-	if len(text) > 0 {
-		return comment + text
-	}
-	return text
-}
-
-func aliasNotoCJKOTC(availFonts ft.Collection) string {
-	comment := "<!-- Alias 'Noto Sans/Serif CJK SC/TC/JP/KR' since they may not installed. -->\n\n"
-	text := ""
-	otcSuffix := []string{" JP", " KR", " SC", " TC"}
-	for _, g := range []string{"Sans", "Serif"} {
-		for _, o := range otcSuffix {
-			name := "Noto " + g + o
-			if len(availFonts.FindByName(name)) <= 0 {
-				continue
-			}
-			text += "\t<alias>\n\t\t<family>Noto " + g + " CJK" + o + "</family>\n\t\t<prefer>\n"
-			text += "\t\t\t<family>" + name + "</family>\n"
-			remain := otcSuffix
-			slice.Remove(&remain, o)
-			for _, r := range remain {
-				text += "\t\t\t<family>Noto " + g + r + "</family>\n"
-			}
-			text += "\t\t</prefer>\n\t</alias>\n\n"
-		}
-	}
 	if len(text) > 0 {
 		return comment + text
 	}
